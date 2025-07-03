48a0e2bde455e7e6b440495312c9a7100a0d7496
Black Boy <2184634+blackboy69@users.noreply.github.com>
2025-07-03 01:06:21 -0700

    Update slideshowpure.js
    Uncommented missed code

--- slideshowpure.js
+++ slideshowpure.js
@@ -1225,10 +1225,8 @@ const SlideshowManager = {
 
         if (!currentSlide) {
           console.error(`Failed to create slide for item ${currentItemId}`);
-          STATE.slideshow.isTransitioning = false;
-          /*lets not spam the server on errors. 
-          setTimeout(() => this.nextSlide(), 500);
-          */
+          STATE.slideshow.isTransitioning = false;          
+          setTimeout(() => this.nextSlide(), 500);          
           return;
         }
       }
